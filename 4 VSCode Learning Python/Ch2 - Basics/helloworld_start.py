7c11add60d81b857b79add8e57013d8f6e1a33fd
luis-fernandezt <luisguillermo.fernandez@outlook.com>
2025-05-30 16:53:31 -0400

    learning-python

---
+++ 4 VSCode Learning Python/Ch2 - Basics/helloworld_start.py
@@ -0,0 +1,16 @@
+#
+# Example file for HelloWorld
+# LinkedIn Learning Python course by Joe Marini
+#
+
+def main():  
+    print("Hello Wolrd")
+    name = input("What is your name?")
+    print("Nice to meet you!", name)
+
+if __name__ == "__main__":
+    main()
+
+# The code above is a simple Python script that prints "Hello World" to the console and then prompts the user for their name, printing a greeting with their name.
+# The script defines a main function that contains the logic, and it checks if the script is being run directly before calling the main function.
+# The script is a simple example of how to use the print and input functions in Python, as well as how to define and call a function.
